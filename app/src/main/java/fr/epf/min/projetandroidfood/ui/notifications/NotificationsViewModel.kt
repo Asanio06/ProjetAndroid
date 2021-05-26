a7d87d614b077873288d54fd9041b5e68152e6ab
unknown <isdore.porretti@epfedu.fr>
2021-05-26 12:11:00 +0200

    'ajout_des_icons_du_bottom_navigator'

---
+++ app/src/main/java/fr/epf/min/projetandroidfood/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package fr.epf.min.projetandroidfood.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
