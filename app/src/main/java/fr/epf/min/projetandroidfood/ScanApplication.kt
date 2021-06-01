106e8fbcbe179d4f18bc9a643d72f0dcc8c36fef
IsidorePorretti <isidore.porretti@outlook.fr>
2021-06-01 22:21:29 +0200

    scan_version_ariefbayu.xyz_mais_pb

---
+++ app/src/main/java/fr/epf/min/projetandroidfood/ScanApplication.kt
@@ -0,0 +1,10 @@
+package fr.epf.min.projetandroidfood
+
+import androidx.multidex.MultiDexApplication
+
+class ScanApplication : MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
