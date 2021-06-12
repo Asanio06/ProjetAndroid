b754d0fe479199b28adb9b8a50d7a6521a4501c3
IsidorePorretti <isidore.porretti@outlook.fr>
2021-06-12 11:28:19 +0200

    'permission'

--- app/src/main/java/fr/epf/min/projetandroidfood/MainActivity2.kt
+++ app/src/main/java/fr/epf/min/projetandroidfood/MainActivity2.kt
@@ -1,8 +1,13 @@
 package fr.epf.min.projetandroidfood
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,6 +17,10 @@ import fr.epf.min.projetandroidfood.databinding.ActivityMain2Binding
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +36,36 @@ class MainActivity2 : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home
-           //     , R.id.navigation_favorite, R.id.navigation_center_focus_weak, R.id.navigation_search
+                //     , R.id.navigation_favorite, R.id.navigation_center_focus_weak, R.id.navigation_search
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        checkPermission( Manifest.permission.CAMERA,  CAMERA_PERMISSION_CODE)
     }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity2, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity2, arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(this@MainActivity2, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity2, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity2, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
